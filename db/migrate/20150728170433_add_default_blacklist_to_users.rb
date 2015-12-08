361626f1fae7cac94ae052d933c20001f8b9a779
taichibana <taichibana@users.noreply.github.com>
2015-12-08 16:24:16 +0100

    Update 20150728170433_add_default_blacklist_to_users.rb

--- db/migrate/20150728170433_add_default_blacklist_to_users.rb
+++ db/migrate/20150728170433_add_default_blacklist_to_users.rb
@@ -1,7 +1,7 @@
 class AddDefaultBlacklistToUsers < ActiveRecord::Migration
   def self.up
     execute "set statement_timeout = 0"
-    blacklist = ["spoilers", "guro", "scat", "furry -rating:s"].join("\n")
+    blacklist = ["spoilers", "guro", "scat"].join("\n")
     change_column_default(:users, :blacklisted_tags, blacklist)
   end
 
